96274dfc60815a2733c5ff904e015b0f1b1f0b99
Tomka Yuriy <tomka.yurii@gmail.com>
2022-06-28 18:47:24 +0300

    second example added

---
+++ WF_example_01_CHECKEDLISTBOX/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WF_example_01_CHECKEDLISTBOX
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddToCheckBox = new System.Windows.Forms.Button();
+            this.btnAddSelected = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(428, 136);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 169);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(428, 144);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(478, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnAddToCheckBox
+            // 
+            this.btnAddToCheckBox.Location = new System.Drawing.Point(478, 115);
+            this.btnAddToCheckBox.Name = "btnAddToCheckBox";
+            this.btnAddToCheckBox.Size = new System.Drawing.Size(286, 84);
+            this.btnAddToCheckBox.TabIndex = 3;
+            this.btnAddToCheckBox.Text = "Add To CheckBox";
+            this.btnAddToCheckBox.UseVisualStyleBackColor = true;
+            this.btnAddToCheckBox.Click += new System.EventHandler(this.btnAddToCheckBox_Click);
+            // 
+            // btnAddSelected
+            // 
+            this.btnAddSelected.Location = new System.Drawing.Point(478, 229);
+            this.btnAddSelected.Name = "btnAddSelected";
+            this.btnAddSelected.Size = new System.Drawing.Size(286, 84);
+            this.btnAddSelected.TabIndex = 4;
+            this.btnAddSelected.Text = "Add Selected";
+            this.btnAddSelected.UseVisualStyleBackColor = true;
+            this.btnAddSelected.Click += new System.EventHandler(this.btnAddSelected_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 337);
+            this.Controls.Add(this.btnAddSelected);
+            this.Controls.Add(this.btnAddToCheckBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddToCheckBox;
+        private System.Windows.Forms.Button btnAddSelected;
+    }
+}
